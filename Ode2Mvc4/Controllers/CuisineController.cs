d7430efc44a168f066ec47d2e92039672f5e4117
Mark Aurit <mark.aurit@outlook.com>
2014-08-21 08:37:06 -0700

    initial create

---
+++ Ode2Mvc4/Controllers/CuisineController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ode2Mvc4.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+            var message = String.Format("{0}::{1} {2}", controller, action, id);
+
+            return Content(message);
+        }
+
+    }
+}
