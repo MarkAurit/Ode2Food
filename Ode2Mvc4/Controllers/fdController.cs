f8a4fe01488880e35d53c50b8ac1f4bd04545a1a
Mark Aurit <mark.aurit@outlook.com>
2014-08-21 09:23:41 -0700

    more returns

--- Ode2Mvc4/Controllers/fdController.cs
+++ Ode2Mvc4/Controllers/fdController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Ode2Mvc4.Controllers
 {
@@ -16,15 +17,25 @@ namespace Ode2Mvc4.Controllers
             return View();
         }
 
-        public ActionResult Search(string name = "German")
+        //5. specify http verb
+        [HttpPost]
+        public ActionResult Search(string name)
         {
             //var controller = RouteData.Values["controller"];
             //var action = RouteData.Values["action"];
             //var id = RouteData.Values["id"];
             //var message = String.Format("{0}::{1} {2}", controller, action, id);
+            
             var message = Server.HtmlEncode(name);
-
+            
+            // 1. display text
             return Content(message);
+            // 2. return to action on controller
+            //return RedirectToAction("Index", "Home", new {name = name});
+            // 3. return to routing table
+            //return RedirectToRoute("Default", new {controller = "Home", action = "About"});
+            // 4. return json
+            //return Json(new {Message = message, time = DateTime.Now.ToShortDateString()}, JsonRequestBehavior.AllowGet);
         }
 
     }
