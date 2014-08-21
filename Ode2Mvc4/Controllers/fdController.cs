8cfc3ab30ff4c75e3f0c3f2f8318bec01b3f68da
Mark Aurit <mark.aurit@outlook.com>
2014-08-21 10:01:07 -0700

    updates

--- Ode2Mvc4/Controllers/fdController.cs
+++ Ode2Mvc4/Controllers/fdController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Ode2Mvc4.Filters;
 
 namespace Ode2Mvc4.Controllers
 {
+    [Log]
     public class fdController : Controller
     {
         //
